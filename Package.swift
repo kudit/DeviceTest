bb810de97516fd7f07e56b5f0cb260932accab3f
kudit <support@kudit.com>
2024-04-04 22:35:18 -0400

    v1.0.4

--- Package.swift
+++ Package.swift
@@ -53,10 +53,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Device",
-            path: "Sources",
-            resources: [
-            	.copy("SymbolAssets.xcassets"),
-	        ]
+            path: "Sources"
         ),
         .executableTarget(
             name: "DeviceTestAppModule",
